38f7074aede82834aa726919d33626140287f3c9
Datseris <datseris.george@gmail.com>
2020-05-03 15:50:41 +0200

    add a 3-component test just in case

--- test/lpv_test.jl
+++ test/lpv_test.jl
@@ -4,7 +4,7 @@ Random.seed!(151512)
 tu = cumsum(rand(L)/4) # non-equispaced timeaxis
 pu = periodicf(tu)
 
-@testset "Sinusoidal" begin
+@testset "Sinusoidal-2" begin
     for (name, re) in zip(("cos(3t)", "gaussian"), (cos.(3tu), noise))
         s = pu .+ re .- mean(re)
         x, r = decompose(tu, s, m)
@@ -24,4 +24,23 @@ pu = periodicf(tu)
         # plot(r; alpha = 0.75, ls = "dashed", label = "err=$errres")
         # ylabel("r"); legend()
     end
+end
+
+@testset "Sinusoidal-3" begin
+    s = pu .+ cos.(3tu) .+ noise
+    x, r = decompose(tu, s, Sinusoidal([1, 2, 3]))
+    errper = nrmse(pu .+ cos.(3tu), x)
+    @test errper < 0.1
+    errres = nrmse(noise, r)
+    @test errres < 0.1
+
+    # figure()
+    # ax1 = subplot(211)
+    # plot(pu; alpha = 0.75, label = "x component")
+    # plot(x; alpha = 0.75, ls = "dashed", label = "err=$errper")
+    # ylabel("x"); legend()
+    # subplot(212; sharex = ax1)
+    # plot(noise; alpha = 0.75, label = "r component")
+    # plot(r; alpha = 0.75, ls = "dashed", label = "err=$errres")
+    # ylabel("r"); legend()
 end
