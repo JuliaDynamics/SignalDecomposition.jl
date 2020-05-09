a06f37d5c59cc0854c1ec0941f5cfa41fcdf46a8
Datseris <datseris.george@gmail.com>
2020-05-10 01:34:41 +0200

    add minor tests

--- src/misc/anomaly.jl
+++ src/misc/anomaly.jl
@@ -9,28 +9,29 @@ Decompose timeseries `s` into its temporal average `x` and the anomalies `r`
 so that `s = x + r`. Each unique day+month combination in `t` is identified, and the
 values of `s` for each combination are averaged
 (no special handling for February is and leap years).
-As a result, the time vector `t` must have dates, i.e. have `eltype <: TimeType`.
+As a result, the time vector `t` must be `<:AbstractVector{<:TimeType}`.
 
 This method is very common in climate science, referred to as simply "anomalies".
 """
 struct TimeAnomaly end
 
-decompose(t, s, method::TimeAnomaly) = error("eltype of `t` must be `<:TimeType`")
+decompose(t, s, method::TimeAnomaly) = error("`t` must be `<:AbstractVector{<:TimeType}`")
 
-function decompose(t::Vector{<:TimeType}, s, method::TimeAnomaly)
+function decompose(t::AbstractVector{<:TimeType}, s, method::TimeAnomaly)
+    @assert length(t) == length(s)
     udates = unique!(sort!(daymonth.(t)))
     counters = Dict(u => [0, zero(eltype(s))] for u in udates)
     x = copy(s)
     # First run: calculate climatology averages
-    for i in 1:length(t)
+    @inbounds for i in 1:length(t)
         date = daymonth(t[i])
         counters[date] .+= (1, s[i])
     end
     # Second run: make x
     for (key, val) in counters
-        val[2] = val[2]/val[1] # take the average
+        @inbounds val[2] = val[2]/val[1] # take the average
     end
-    for i in 1:length(t)
+    @inbounds for i in 1:length(t)
         date = daymonth(t[i])
         x[i] = counters[date][2]
     end
---
+++ test/timeanomaly_test.jl
@@ -0,0 +1,28 @@
+using Dates, Random
+
+@testset "TimeAnomaly" begin
+    Random.seed!(41516)
+    y = Date(2001):Day(1):Date(2025)
+    dy = dayofyear.(y)
+    cy =  @. 4 + 7.2cos(2π*dy/365.26) + 5.6cos(4π*dy/365.26 + 3π/5)
+    r0 = randn(length(dy))/2
+    sy = cy .+ r0
+
+    x, r = decompose(y, sy, TimeAnomaly())
+
+    errper = nrmse(cy, x)
+    @test errper < 0.1
+    errres = nrmse(r0, r)
+    @test nrmse(r0, r) < 0.5
+
+    # figure()
+    # title("TimeAnomaly")
+    # ax1 = subplot(211)
+    # plot(y, cy; alpha = 0.75, label = "original")
+    # plot(y, x; alpha = 0.75, ls = "dashed", label = "err=$errper")
+    # ylabel("x"); legend()
+    # subplot(212; sharex = ax1)
+    # plot(y, r0; alpha = 0.75, label = "original")
+    # plot(y, r; alpha = 0.75, ls = "dashed", label = "err=$errres")
+    # ylabel("r"); legend()
+end
