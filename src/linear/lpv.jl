4edda6d9716e883eee6509afbf0598d3fdbad3a8
Datseris <datseris.george@gmail.com>
2020-05-09 16:56:03 +0200

    use mean, expose fitted coefficients

--- src/linear/lpv.jl
+++ src/linear/lpv.jl
@@ -5,43 +5,42 @@ export Sinusoidal
     Sinusoidal(fs, x=true)
 Decompose a timeseries `s` into a **sum** `x + r`, where `x`
 are sinusoidal components with the given frequencies `fs` that minimize
-coefficients ``A_i, \\phi_i`` of the expression
+coefficients ``A, \\phi`` of the expression
 ```math
-s - \\bar{s} \\approx \\sum_i A_i \\cos(2\\pi f_i t + \\phi_i)
+s \\approx A_0 + \\sum_i A_i \\cos(2\\pi f_i t + \\phi_i)
 ```
 with ``\\bar{s}`` the mean.
-There is arbitrarity of which part of the signal `x, r`
-gets the mean value of `s`, because it is deducted for a better fit.
-The argument `x=true` attributes it to `x`, use `false` for `r`.
 
 This method uses a new least-squares algorithm in frequency domain using the package
 [LPVSpectral.jl](https://github.com/baggepinnen/LPVSpectral.jl), see[^Bagge2017].
-
-This method works for non-equispaced `t` axis (and also normal), is generally very accurate
+It works for non-equispaced `t` axis (and also normal), is generally very accurate
 (if choosen frequencies are not too close), but has performance scaling of
 O(N^2.4) instead of O(n log(n)) of [`Fourier`](@ref).
 
-[^Bagge2018]: F. Bagge Carlson et al., [Linear Parameter-Varying Spectral Decomposition](https://lup.lub.lu.se/search/publication/ac32368e-e199-44ff-b76a-36668ac7d595), 2017.
+Because it can work with arbitrary signal length the method always estimates the
+zero-frequency Fourier component, and attributes it to `x`.
+The fitted coefficients ``A, \\phi`` are available as fields `.A` and `.φ` of the struct
+(first entry is zero-frequency component).
+
+[^Bagge2017]: F. Bagge Carlson et al., [Linear Parameter-Varying Spectral Decomposition](https://lup.lub.lu.se/search/publication/ac32368e-e199-44ff-b76a-36668ac7d595).
 """
 struct Sinusoidal{F<:AbstractArray{<:AbstractFloat}} <: Decomposition
     fs::F
-    x::Bool
+    A::Vector{Float64}
+    φ::Vector{Float64}
 end
-Sinusoidal(fs) = Sinusoidal(fs, true)
+Sinusoidal(fs) = Sinusoidal(fs, zeros(length(fs)+1), zeros(length(fs)+1))
 
 function decompose(t, s, method::Sinusoidal)
-    fs = method.fs
-    μ = mean(s)
-    χ, = ls_spectral(s .- μ, t, fs)
+    fs = [0, method.fs...]
+    χ, = ls_spectral(s, t, fs)
     As = abs.(χ) ./ sqrt(2*length(fs))
     φs = angle.(χ)
-    x = zero(s)
-    for i in 1:length(fs)
+    method.A .= As
+    method.φ .= φs
+    x = fill(As[1], length(s))
+    for i in 2:length(fs)
         @. x += As[i]*cos(2π*fs[i]*t + φs[i])
     end
-    if method.x
-        return x .+ μ, s .- x .- μ
-    else
-        return x, s .- x
-    end
+    return x, s .- x
 end
