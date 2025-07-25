591934c90a12ffd7bf07cf7b7f29b32e59ce040b
George Datseris <datseris.george@gmail.com>
2025-07-25 10:25:13 +0100

    add delay embedding in test

--- test/nonlinear_test.jl
+++ test/nonlinear_test.jl
@@ -1,3 +1,6 @@
+import DelayEmbeddings
+using Test, SignalDecomposition
+
 @testset "ExtremelySimpleNL" begin
 # input
 s1 = lorenzx + 0.1noise # input
@@ -10,7 +13,7 @@ w = 1 # theiler window
 
 for (name, s) in zip(("lorenz", "roessler"), (s1, s2))
 
-    τ = estimate_delay(s, "mi_min") #  5 # delaytime
+    τ = DelayEmbeddings.estimate_delay(s, "mi_min") #  5 # delaytime
     method = ExtremelySimpleNL(k, ℓ, τ, w, ε)
 
     x, r = decompose(s, method)
